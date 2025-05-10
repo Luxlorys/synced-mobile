ef29bdc364cd3e474de820d300627c619918da1b
Andrei <andrei@MacBook-Pro-Andrei.local>
2025-05-10 19:06:22 +0300

    feat: base navigator

--- src/shared/services/RouteService/models/RootStackParamList.ts
+++ src/shared/services/RouteService/models/RootStackParamList.ts
@@ -4,15 +4,21 @@ import { Routes } from './Routes';
 export type RouteType = keyof typeof Routes;
 
 export type RootStackParamList = {
-  [Routes.MAIN_NAVIGATOR]: undefined;
-
   [Routes.AUTH_NAVIGATOR]: undefined;
+  [Routes.WELCOME]: undefined;
+  [Routes.SIGN_IN]: undefined;
+  [Routes.SIGN_UP_COMPANY_INFO]: undefined;
+  [Routes.SIGN_UP_ENTER_INVITE_CODE]: undefined;
+  [Routes.SIGN_UP_MAIN]: undefined;
+  [Routes.SIGN_UP_PARTICIPANT]: undefined;
 
   [Routes.BOTTOM_TAB_BAR_NAVIGATOR]: undefined;
 
-  [Routes.ALERTS_NAVIGATOR]: undefined;
+  [Routes.MAIN_NAVIGATOR]: undefined;
+  [Routes.HOME]: undefined;
 
-  [Routes.PROFILE_NAVIGATOR]: undefined;
+  [Routes.SETTINGS_NAVIGATOR]: undefined;
+  [Routes.SETTINGS]: undefined;
 
   [Routes.AUTH]: undefined;
 
